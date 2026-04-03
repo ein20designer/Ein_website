2a93965f08b93ac3f1f36c824410e1a9ede090d0
ein20designer <Ray20designer@gmail.com>
2026-04-03 23:25:32 +0300

    fix: add base path for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
+
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/Ein_website/', // <-- أضف هذا السطر هنا بالضبط
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
